9c1ec17ccd6e36a82b37f147b297a49a5ec66faa
Victor Defauchy <victor.defauchy@epitech.eu>
2026-03-01 00:34:21 +0100

    FIX: rollback to criterion

--- Start/include/tests.h
+++ Start/include/tests.h
@@ -7,13 +7,11 @@
 
 #ifndef TEST_H
     #define TEST_H
-    #include <stddef.h>
-    #include <stdarg.h>
-    #include <setjmp.h>
-    #include <cmocka.h>
+    #include <criterion/criterion.h>
+    #include <criterion/redirect.h>
     #include "my.h"
 
-// MAIN
-char *capture_stdout(void (*func)(void));
+// TOOL
+void redirect_all_std(void);
 
 #endif /* TEST_H */
---
+++ Start/tests/test_tool.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2026
+** [project]
+** File description:
+** TESTS
+*/
+
+#include "tests.h"
+
+void redirect_all_std(void)
+{
+    cr_redirect_stdout();
+    cr_redirect_stderr();
+}
