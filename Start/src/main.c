5cd02eaa5d2bff3b7b9b23543cbd70b9e9ec8e15
Victor Defauchy <victor.defauchy@epitech.eu>
2026-04-11 13:28:57 +0200

    ADD: src into start

---
+++ Start/src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** [project]
+** File description:
+** Descrption
+*/
+
+#include "my.h"
+
+int main(int ac, char **av)
+{
+    return project_(ac, av);
+}
---
+++ Start/src/project.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** [project]
+** File description:
+** Descrption
+*/
+
+#include "my.h"
+
+int project_(int ac, char **av)
+{
+    return SUCCESS;
+}
